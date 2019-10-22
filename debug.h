bc4e8c587992fd725478270e83ceb27589daf884
supertom-spec <supertom@tutanota.com>
2019-10-22 16:52:01 +0800

    debug

--- debug.h
+++ debug.h
@@ -3,12 +3,21 @@
 
 typedef unsigned char BYTE;
 
+int debug_flag = 1;
+
 void debug_msg(const char *msg) {
+    if ( !debug_flag ) {
+        return;
+    }
     printf("[+] %s\n", msg);
 }
 
 void debug_hex(const char *msg, BYTE* addr, int n) {
+    if ( !debug_flag ) {
+        return;
+    }
     printf("[+] Memory of 0x%X (%s): \n", addr, msg);
+    printf("0000:  ");
     for (int i = 0; i < n; i++) {
         printf("%02x", *(addr+i));
         if ( (i+1) % 2 == 0 ) {
@@ -18,18 +27,18 @@ void debug_hex(const char *msg, BYTE* addr, int n) {
             printf("  ");
         }
         if ( (i+1) % 16 == 0 ) {
-            printf("\n");
+            printf("\n%04X:  ", i+1);
         }
     }
     printf("\n");
 }
-
+/*
 typedef struct debug {
     int flag;
     void (*msg)(const char *msg);
     void (*hex)(const char *msg, BYTE* addr, int n);
 } debug;
-
-debug d = { 1, msg:debug_msg, hex:debug_hex };
+*/
+//debug d = { 1, msg:debug_msg, hex:debug_hex };
 
 #endif /* _DEBUG_H_ */
